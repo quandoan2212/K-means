f2d2be6a2458b9cce5ea521dc2dd140f07b3aa1a
quandoan2212 <1851010108quan@ou.edu.vn>
2022-02-24 23:05:36 +0700

    'update2functions:measure_distance_to_centr

--- kMeans.py
+++ kMeans.py
@@ -1,19 +1,54 @@
 from components.point import Point
+from numpy import mean
+
 class KMeans:
 
-    def __init__(self, K, measure_type, list_points, first_centr, second_centr):
+    def __init__(self, K, measure_type, list_points, list_centr):
         self.K = K
         self.measure_type = measure_type
         self.list_points = list_points
-        self.first_centr = first_centr
-        self.second_centr = second_centr
+        if len(list_centr) == K:
+            self.list_centr = list_centr
+        else:
+            raise Exception("==== Number of initial centroids must equal to K value ====")
+
+    def solveKmeans(self):
+        pass
 
     def measure_distance_to_centr(self):
-        result = []
+        result = list()
+
+        #Calculate the matrix contains distance values of all point in list to centroid points in the list
+        point_distance = list()
         for p in self.list_points:
-            d_one = p.measure_distance_to_other_point(self.first_centr)
-            d_two = p.measure_distance_to_other_point(self.second_centr)
-            distance_point = Point(format("%s to centroid", p.name), d_one, d_two)
-            result.append(distance_point)
+            point_distance.clear()
+            for centr in self.list_centr:
+                distance = p.measure_distance_to_other_point(centr, self.measure_type)
+                point_distance.append(distance)
+            result.append(point_distance)
         return result
+
+    def grouping_points(self, list_distance):
+        result = list()
+        subset = dict()
+        for centr in self.list_centr:
+            subset.clear()
+            subset["name"] = centr.name
+            subset["points"] = list()
+            position = self.list_centr.index(centr)
+            for p in list_distance:
+                if p.index(min(p)) == position:
+                    subset["points"].append(p)
+                    list_distance.remove(p)
+            result.append(subset)
+        return result
+
+
+    def calc_average(self):
+        pass
+
+
+
+
+
 

ba465ce3c9709c659814f673b05f7e3ce1a53215
quandoan2212 <1851010108quan@ou.edu.vn>
2022-02-26 16:35:08 +0700

    'updatemain'

--- main.py
+++ main.py
@@ -1,43 +1,14 @@
 from components.point import Point
-# from operator import eq
-#
-# a = Point("A", 1, 3)
-# b = Point("B", 2, 4)
-#
-# d_a_to_b = a.measure_distance_to_other_point(b, 1)
-# print(d_a_to_b)
-# import numpy
-#
-# list_temp = [1, 2, 3]
-# res = numpy.mean([x for x in list_temp])
-# print(res)
-
-
-# b = {
-#     "name": "A1",
-#     "points": [0]
-# }
-# c = {
-#     "name": "A2",
-#     "points": [1, 2, 3]
-# }
-# a = [b, c]
-#
-# d = {
-#     "name": "A1",
-#     "points": [0]
-# }
-# e = {
-#     "name": "A2",
-#     "points": [1, 2, 3]
-# }
-# f = [d, e]
-# print(a == f)
-
 from kMeans import KMeans
+
 problem = KMeans(K=2, measure_type=2, list_centr=[Point("C1", 1, 1), Point("C2", 2, 1)],
                  list_points=[Point("A", 1, 1), Point("B", 2, 1),
                               Point("C", 4, 3), Point("D", 5, 4)])
+
+# problem = KMeans(K=2, measure_type=2, list_centr=[Point("C1", 3, 2), Point("C2", 3, 1)],
+#                  list_points=[Point("A", 1, 1), Point("B", 0, 2),
+#                               Point("C", 2, 2), Point("D", 1, -1),
+#                               Point("E", -1, -1), Point("F", 1, -2)])
 result = problem.solveKmeans()
 print(result)
 for p in result[2]:
